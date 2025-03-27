e03c9aef62f4e996569a23cdbac0441c0c735e40
Clemsimao <simaoclement33@gmail.com>
2025-03-27 21:21:02 +0100

    chore: install sequelize

---
+++ app/sequelizeClient.js
@@ -0,0 +1,9 @@
+import { Sequelize } from 'sequelize';
+
+export const sequelize = new Sequelize(process.env.PG_URL, {
+    define: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        underscored: true,
+    },
+});
