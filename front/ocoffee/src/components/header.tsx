374d1727031d7c527432191d797375184113b61c
Clemsimao <simaoclement33@gmail.com>
2025-05-25 21:28:58 +0200

    maj: menu burger in responsive

--- front/ocoffee/src/components/header.tsx
+++ front/ocoffee/src/components/header.tsx
@@ -1,22 +1,27 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => setMenuOpen(open => !open);
+
     return (
         <header>
         <a className="header-logo" href="/">
             <img src="/images/logo.svg" alt="logo d'O'Coffee"/>
         </a>
         <a className="name-logo" href="/">O'Coffee</a>
-        <div className="menu-burger">
+        <div className="menu-burger" onClick={toggleMenu} aria-label="Ouvrir le menu" tabIndex={0}>
             <span className="burger-line"></span>
             <span className="burger-line"></span>
             <span className="burger-line"></span>
         </div>
-        <nav className="main-menu">
+        <nav className={`main-menu${menuOpen ? " open" : ""}`}>
             <ul>
-                <li><Link to="/">Accueil</Link></li>
-                <li><Link to="/catalogue">Catalogue</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/" onClick={() => setMenuOpen(false)}>Accueil</Link></li>
+                <li><Link to="/catalogue" onClick={() => setMenuOpen(false)}>Catalogue</Link></li>
+                <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
             </ul>
         </nav>
     </header>
